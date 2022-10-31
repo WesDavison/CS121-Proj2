2cec3b4342ad7d036f61d1c718b5479a1380f5d6
Wesley Davison <wdavison@uci.edu>
2022-10-30 19:28:21 -0700

    small edit

--- scraper.py
+++ scraper.py
@@ -116,7 +116,8 @@ def extract_next_links(url, resp):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", web_type.path.lower()) != None:
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|apk)$", web_type.path.lower()) != None:
+            badURLs.add(urlNoFrag)
             return list()
         
         goodWordCountRange = reportGeneration(resp.url, resp)
